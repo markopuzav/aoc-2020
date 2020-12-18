2aa93a74740a32783ff60362bfed3ec065479c51
Marko Puza <markopuza@gmail.com>
2020-12-18 10:09:39 +0100

    better day 18

--- day18/solution.py
+++ day18/solution.py
@@ -1,63 +1,45 @@
 from enum import Enum
-from typing import Union
-# pylint: disable=unsubscriptable-object
-Instructions = list[Union[int, str]]
+
 Expression = str
 
 class Precedence(Enum):
     EQUAL = 'EQUAL'
     ADDITION_FIRST = 'ADDITION_FIRST'
 
-# global precedence
-PRECEDENCE = Precedence.EQUAL
-
 class N():
-    def __init__(self, n: int=None, instructions: Instructions=None) -> None:
-        self.instructions = instructions or [n]
-    
+    def __init__(self, n: int) -> None:
+        self.n = n
+
     def __add__(self, other: 'N') -> 'N':
-        return N(instructions=self.instructions + ['+'] + other.instructions)
+        return N(self.n + other.n)
+
+    def __sub__(self, other: 'N') -> 'N':
+        return N(self.n * other.n)
 
     def __mul__(self, other: 'N') -> 'N':
-        result = N(instructions=self.instructions + ['*'] + other.instructions)
-        if PRECEDENCE == Precedence.EQUAL:
-            return result
-        elif PRECEDENCE == Precedence.ADDITION_FIRST:
-             # in this mode addition is denoted as *, so Python will call this function first.
-             # Evaluate greedily.
-            return _(result)
+        return N(self.n + other.n)
 
     def __str__(self) -> str:
-        return str(self.instructions)
-
-def _(n: N) -> N:
-    ''' N-evaluation operator '''
-    res = n.instructions[0]
-    for op, nxt in list(zip(n.instructions[1:], n.instructions[2:]))[::2]:
-        addition_op = {Precedence.EQUAL: '+', Precedence.ADDITION_FIRST: '*'}[PRECEDENCE]
-        res = res + nxt if op == addition_op else res * nxt
-    return N(res)
+        return str(self.n)
 
 def get_input() -> list[Expression]:
     with open('input.txt', 'r') as f:
         return [l.strip() for l in f if l]
 
-def evaluate(expression: Expression) -> int:
-    s = '_(' + expression.replace('(', '_(') + ')'
+def evaluate(expression: Expression, precedence=Precedence.EQUAL) -> int:
+    s = expression
     for d in range(10):
         s = s.replace(f'{d}', f'N({d})')
-    if PRECEDENCE == Precedence.ADDITION_FIRST: # switcheroo
-        s = ''.join({'+': '*', '*': '+'}[ch] if ch in '+*' else ch for ch in s)
-    return eval(s).instructions[0]
-
+    s = s.replace('*', '-')
+    if precedence == Precedence.ADDITION_FIRST:
+        s = s.replace('+', '*')
+    return eval(s).n
 
 def part_1(expressions) -> int:
     return sum(evaluate(e) for e in expressions)
 
 def part_2(expressions) -> int:
-    global PRECEDENCE
-    PRECEDENCE = Precedence.ADDITION_FIRST
-    return sum(evaluate(e) for e in expressions)
+    return sum(evaluate(e, Precedence.ADDITION_FIRST) for e in expressions)
 
 if __name__ == "__main__":
     expressions = get_input()
